2b769fdc7a7927c07f75204a2f5abc9dbb680c19
Rodrigo Pozo Sánchez <120724679+rodrigopozodev@users.noreply.github.com>
2024-07-15 20:28:59 +0200

    Inicia commit

---
+++ script.js
@@ -0,0 +1,525 @@
+const data = {
+    "Brazos": {
+        "Bíceps": {
+            "Bíceps braquial": [
+                {
+                    ejercicio: "Curl de Bíceps con Barra",
+                    video: "https://youtube.com/shorts/veOoIkW6Spw?si=PMWcvouLa_ruLhq2"
+                }
+            ],
+            "Braquial anterior": [
+                {
+                    ejercicio: "Curl Martillo",
+                    video: "https://youtube.com/shorts/1pTUHKXGaSs?si=KUj4KDKJXSseTwxd"
+                }
+            ]
+        },
+        "Tríceps": {
+            "Tríceps braquial (cabeza larga)": [
+                {
+                    ejercicio: "extensión de tríceps por encima de la cabeza con mancuernas",
+                    video: ""
+                }
+            ],
+            "Tríceps braquial (cabeza lateral)": [
+                {
+                    ejercicio: "Press de Banca con Agarre Cerrado",
+                    video: "https://youtube.com/shorts/kCuB4PfbMPg?si=piTsHzQnz-6DCDXU"
+                }
+            ],
+            "Tríceps braquial (cabeza media)": [
+                {
+                    ejercicio: "Press Frances",
+                    video: "https://youtube.com/shorts/X3-dKjxe_GU?si=21hmjrg-DmLTvpou"
+                }
+            ]
+        },
+        "Antebrazo": {
+            "Pronador redondo": [
+                {
+                    ejercicio: "Pronación de muñeca con mancuerna",
+                    video: "https://www.youtube.com/embed/VIDEO_ID7"
+                }
+            ],
+            "Flexor radial del carpo": [
+                {
+                    ejercicio: "Flexión de muñeca con barra",
+                    video: "https://www.youtube.com/embed/VIDEO_ID8"
+                }
+            ],
+            "Flexor cubital del carpo": [
+                {
+                    ejercicio: "Flexión ulnar del carpo con barra",
+                    video: "https://www.youtube.com/embed/VIDEO_ID9"
+                }
+            ],
+            "Flexor superficial de los dedos": [
+                {
+                    ejercicio: "Flexión de dedos con barra",
+                    video: "https://www.youtube.com/embed/VIDEO_ID10"
+                }
+            ],
+            "Flexor profundo de los dedos": [
+                {
+                    ejercicio: "Flexión profunda de dedos con mancuerna",
+                    video: "https://www.youtube.com/embed/VIDEO_ID11"
+                }
+            ],
+            "Flexor largo del pulgar": [
+                {
+                    ejercicio: "Flexión de pulgar con gomas",
+                    video: "https://www.youtube.com/embed/VIDEO_ID12"
+                }
+            ],
+            "Pronador cuadrado": [
+                {
+                    ejercicio: "Pronación de muñeca con polea",
+                    video: "https://www.youtube.com/embed/VIDEO_ID13"
+                }
+            ],
+            "Extensor radial corto del carpo": [
+                {
+                    ejercicio: "Extensión radial corta de muñeca con mancuerna",
+                    video: "https://www.youtube.com/embed/VIDEO_ID14"
+                }
+            ],
+            "Extensor radial largo del carpo": [
+                {
+                    ejercicio: "Extensión radial larga de muñeca con barra",
+                    video: "https://www.youtube.com/embed/VIDEO_ID15"
+                }
+            ],
+            "Extensor cubital del carpo": [
+                {
+                    ejercicio: "Extensión ulnar del carpo con barra",
+                    video: "https://www.youtube.com/embed/VIDEO_ID16"
+                }
+            ],
+            "Extensor de los dedos": [
+                {
+                    ejercicio: "Extensión de dedos con gomas",
+                    video: "https://www.youtube.com/embed/VIDEO_ID17"
+                }
+            ],
+            "Extensor del dedo meñique": [
+                {
+                    ejercicio: "Extensión del meñique con polea",
+                    video: "https://www.youtube.com/embed/VIDEO_ID18"
+                }
+            ]
+        }
+    },
+    "Hombros": {
+        "Deltoides (anterior)": {
+            "Deltoides anterior": [
+                {
+                    ejercicio: "Press militar con barra",
+                    video: "https://www.youtube.com/embed/VIDEO_ID19"
+                }
+            ]
+        },
+        "Deltoides (lateral)": {
+            "Deltoides medio": [
+                {
+                    ejercicio: "Elevaciones laterales con mancuernas",
+                    video: "https://www.youtube.com/embed/VIDEO_ID20"
+                }
+            ]
+        },
+        "Deltoides (posterior)": {
+            "Deltoides posterior": [
+                {
+                    ejercicio: "Pájaros con mancuernas",
+                    video: "https://www.youtube.com/embed/VIDEO_ID21"
+                }
+            ]
+        },
+        "Supraespinoso": {
+            "Elevación de hombros con mancuernas": [
+                {
+                    ejercicio: "Elevación de hombros con mancuernas",
+                    video: "https://www.youtube.com/embed/VIDEO_ID22"
+                }
+            ]
+        },
+        "Infraespinoso": {
+            "Rotación externa de hombros con mancuernas": [
+                {
+                    ejercicio: "Rotación externa de hombros con mancuernas",
+                    video: "https://www.youtube.com/embed/VIDEO_ID23"
+                }
+            ]
+        },
+        "Redondo menor": {
+            "Apertura de hombros con mancuernas": [
+                {
+                    ejercicio: "Apertura de hombros con mancuernas",
+                    video: "https://www.youtube.com/embed/VIDEO_ID24"
+                }
+            ]
+        },
+        "Redondo mayor": {
+            "Apertura de hombros con mancuernas": [
+                {
+                    ejercicio: "Apertura de hombros con mancuernas",
+                    video: "https://www.youtube.com/embed/VIDEO_ID25"
+                }
+            ]
+        },
+        "Subescapular": {
+            "Abducción de hombros con mancuernas": [
+                {
+                    ejercicio: "Abducción de hombros con mancuernas",
+                    video: "https://www.youtube.com/embed/VIDEO_ID26"
+                }
+            ]
+        }
+    },
+    "Pecho": {
+        "Pectoral mayor (porción clavicular)": {
+            "Press de banca inclinado con barra": [
+                {
+                    ejercicio: "Press de banca inclinado con barra",
+                    video: "https://www.youtube.com/embed/VIDEO_ID27"
+                }
+            ]
+        },
+        "Pectoral mayor (porción esternal)": {
+            "Press de banca plano con barra": [
+                {
+                    ejercicio: "Press de banca plano con barra",
+                    video: "https://www.youtube.com/embed/VIDEO_ID28"
+                }
+            ]
+        },
+        "Pectoral mayor (porción abdominal)": {
+            "Press de banca declinado con barra": [
+                {
+                    ejercicio: "Press de banca declinado con barra",
+                    video: "https://www.youtube.com/embed/VIDEO_ID29"
+                }
+            ]
+        },
+        "Pectoral menor": {
+            "Flexiones de pecho en suelo": [
+                {
+                    ejercicio: "Flexiones de pecho en suelo",
+                    video: "https://www.youtube.com/embed/VIDEO_ID30"
+                }
+            ]
+        },
+        "Serrato anterior": {
+            "Flexiones de pecho con palmada": [
+                {
+                    ejercicio: "Flexiones de pecho con palmada",
+                    video: "https://www.youtube.com/embed/VIDEO_ID31"
+                }
+            ]
+        }
+    },
+    "Espalda": {
+        "Superiores": {
+            "Trapecio (porción superior)": [
+                {
+                    ejercicio: "Encogimientos de hombros con mancuernas",
+                    video: "https://www.youtube.com/embed/VIDEO_ID32"
+                }
+            ],
+            "Trapecio (porción medio)": [
+                {
+                    ejercicio: "Encogimientos de hombros con mancuernas",
+                    video: "https://www.youtube.com/embed/VIDEO_ID32"
+                }
+            ],
+            "Trapecio (porción media)": [
+                {
+                    ejercicio: "Remo con barra",
+                    video: "https://www.youtube.com/embed/VIDEO_ID33"
+                }
+            ]
+        },
+        "Medios e Inferiores": {
+            "Dorsal ancho": [
+                {
+                    ejercicio: "Dominadas en barra fija",
+                    video: "https://www.youtube.com/embed/VIDEO_ID34"
+                }
+            ],
+            "Erectores espinales (iliocostal, longísimo, espinal)": [
+                {
+                    ejercicio: "Peso muerto con barra",
+                    video: "https://www.youtube.com/embed/VIDEO_ID35"
+                }
+            ]
+        }
+    },
+    "Abdomen": {
+        "Recto abdominal": {
+            "Elevaciones de piernas colgado": [
+                {
+                    ejercicio: "Elevaciones de piernas colgado",
+                    video: "https://www.youtube.com/embed/VIDEO_ID36"
+                }
+            ]
+        },
+        "Oblicuo externo": {
+            "Crunch oblicuo en suelo": [
+                {
+                    ejercicio: "Crunch oblicuo en suelo",
+                    video: "https://www.youtube.com/embed/VIDEO_ID37"
+                }
+            ]
+        },
+        "Oblicuo interno": {
+            "Plancha lateral con elevación de cadera": [
+                {
+                    ejercicio: "Plancha lateral con elevación de cadera",
+                    video: "https://www.youtube.com/embed/VIDEO_ID38"
+                }
+            ]
+        },
+        "Transverso abdominal": {
+            "Elevación de piernas en banco declinado": [
+                {
+                    ejercicio: "Elevación de piernas en banco declinado",
+                    video: "https://www.youtube.com/embed/VIDEO_ID39"
+                }
+            ]
+        }
+    },
+    "Piernas": {
+        "Cuádriceps": {
+            "Recto femoral": [
+                {
+                    ejercicio: "Sentadillas con barra",
+                    video: "https://www.youtube.com/embed/VIDEO_ID40"
+                }
+            ],
+            "Vasto lateral": [
+                {
+                    ejercicio: "Sentadillas frontales con barra",
+                    video: "https://www.youtube.com/embed/VIDEO_ID41"
+                }
+            ],
+            "Vasto medial": [
+                {
+                    ejercicio: "Sentadillas con mancuernas",
+                    video: "https://www.youtube.com/embed/VIDEO_ID42"
+                }
+            ],
+            "Vasto intermedio": [
+                {
+                    ejercicio: "Sentadillas con barra guiada",
+                    video: "https://www.youtube.com/embed/VIDEO_ID43"
+                }
+            ]
+        },
+        "Isquiotibiales": {
+            "Bíceps femoral (cabeza larga)": [
+                {
+                    ejercicio: "Peso muerto rumano con barra",
+                    video: "https://www.youtube.com/embed/VIDEO_ID44"
+                }
+            ],
+            "Bíceps femoral (cabeza corta)": [
+                {
+                    ejercicio: "Flexión de piernas tumbado",
+                    video: "https://www.youtube.com/embed/VIDEO_ID45"
+                }
+            ],
+            "Semitendinoso": [
+                {
+                    ejercicio: "Peso muerto a una pierna",
+                    video: "https://www.youtube.com/embed/VIDEO_ID46"
+                }
+            ],
+            "Semimembranoso": [
+                {
+                    ejercicio: "Flexión de piernas en máquina",
+                    video: "https://www.youtube.com/embed/VIDEO_ID47"
+                }
+            ]
+        },
+        "Aductores": {
+            "Aductor mayor": [
+                {
+                    ejercicio: "Sentadillas sumo con barra",
+                    video: "https://www.youtube.com/embed/VIDEO_ID48"
+                }
+            ],
+            "Aductor largo": [
+                {
+                    ejercicio: "Elevaciones laterales de pierna tumbado",
+                    video: "https://www.youtube.com/embed/VIDEO_ID49"
+                }
+            ],
+            "Aductor corto": [
+                {
+                    ejercicio: "Sentadillas con banda elástica",
+                    video: "https://www.youtube.com/embed/VIDEO_ID50"
+                }
+            ],
+            "Pectíneo": [
+                {
+                    ejercicio: "Elevación de piernas con polea baja",
+                    video: "https://www.youtube.com/embed/VIDEO_ID51"
+                }
+            ],
+            "Grácil": [
+                {
+                    ejercicio: "Desplantes laterales",
+                    video: "https://www.youtube.com/embed/VIDEO_ID52"
+                }
+            ]
+        },
+        "Glúteos": {
+            "Glúteo mayor": [
+                {
+                    ejercicio: "Puentes de glúteos con barra",
+                    video: "https://www.youtube.com/embed/VIDEO_ID53"
+                }
+            ],
+            "Glúteo medio": [
+                {
+                    ejercicio: "Patada de glúteo con banda elástica",
+                    video: "https://www.youtube.com/embed/VIDEO_ID54"
+                }
+            ],
+            "Glúteo menor": [
+                {
+                    ejercicio: "Peso muerto sumo con barra",
+                    video: "https://www.youtube.com/embed/VIDEO_ID55"
+                }
+            ]
+        },
+        "Pantorrillas": {
+            "Gastrocnemio (gemelo)": [
+                {
+                    ejercicio: "Elevación de talones de pie",
+                    video: "https://www.youtube.com/embed/VIDEO_ID56"
+                }
+            ],
+            "Sóleo": [
+                {
+                    ejercicio: "Elevación de talones sentado",
+                    video: "https://www.youtube.com/embed/VIDEO_ID57"
+                }
+            ]
+        },
+        "Tibial": {
+            "Tibial anterior": [
+                {
+                    ejercicio: "Elevación de talones con peso",
+                    video: "https://www.youtube.com/embed/VIDEO_ID58"
+                }
+            ]
+        }
+    },
+    "Cuello": {
+        "Esternocleidomastoideo": {
+            "Flexión lateral de cuello con peso": [
+                {
+                    ejercicio: "Flexión lateral de cuello con peso",
+                    video: "https://www.youtube.com/embed/VIDEO_ID59"
+                }
+            ]
+        },
+        "Escalenos (anterior)": {
+            "Flexión de cuello con manos en la cabeza": [
+                {
+                    ejercicio: "Flexión de cuello con manos en la cabeza",
+                    video: "https://www.youtube.com/embed/VIDEO_ID60"
+                }
+            ]
+        },
+        "Escalenos (medio)": {
+            "Flexión de cuello con manos en la nuca": [
+                {
+                    ejercicio: "Flexión de cuello con manos en la nuca",
+                    video: "https://www.youtube.com/embed/VIDEO_ID61"
+                }
+            ]
+        },
+        "Escalenos (posterior)": {
+            "Flexión de cuello con manos en la sien": [
+                {
+                    ejercicio: "Flexión de cuello con manos en la sien",
+                    video: "https://www.youtube.com/embed/VIDEO_ID62"
+                }
+            ]
+        },
+        "Trapecio (porción superior)": {
+            "Encogimientos de hombros con mancuernas": [
+                {
+                    ejercicio: "Encogimientos de hombros con mancuernas",
+                    video: "https://www.youtube.com/embed/VIDEO_ID63"
+                }
+            ]
+        }
+    },
+    "Otros": {
+        "Musculatura profunda del tronco (core)": {
+            "Multífidos": [
+                {
+                    ejercicio: "Extensión de espalda en máquina",
+                    video: "https://www.youtube.com/embed/VIDEO_ID64"
+                }
+            ],
+            "Cuadrado lumbar": [
+                {
+                    ejercicio: "Hiperextensiones de espalda en banco",
+                    video: "https://www.youtube.com/embed/VIDEO_ID65"
+                }
+            ]
+        }
+    }
+};
+
+function updateMuscleGroups() {
+    const bodyPart = document.getElementById('bodyPart').value;
+    const muscleGroupSelect = document.getElementById('muscleGroup');
+    muscleGroupSelect.innerHTML = '<option value="">Selecciona un grupo muscular...</option>';
+    
+    // Limpiar resultados al cambiar de parte del cuerpo
+    const resultsContainer = document.getElementById('results');
+    resultsContainer.innerHTML = '';
+
+    if (bodyPart && data[bodyPart]) {
+        const muscleGroups = Object.keys(data[bodyPart]);
+        muscleGroups.forEach(group => {
+            const option = document.createElement('option');
+            option.value = group;
+            option.textContent = group;
+            muscleGroupSelect.appendChild(option);
+        });
+    }
+}
+
+function showMuscles() {
+    const bodyPart = document.getElementById('bodyPart').value;
+    const muscleGroup = document.getElementById('muscleGroup').value;
+    const resultsContainer = document.getElementById('results');
+    resultsContainer.innerHTML = '';
+
+    if (bodyPart && muscleGroup && data[bodyPart] && data[bodyPart][muscleGroup]) {
+        const muscles = data[bodyPart][muscleGroup];
+        for (const muscle in muscles) {
+            const exercises = muscles[muscle];
+            const muscleDiv = document.createElement('div');
+            muscleDiv.classList.add('muscle');
+            muscleDiv.innerHTML = `<h3>${muscle}</h3>`;
+
+            exercises.forEach(exercise => {
+                const exerciseDiv = document.createElement('div');
+                exerciseDiv.classList.add('exercise');
+                exerciseDiv.innerHTML = `
+                    <h4>${exercise.ejercicio}</h4>
+                    <a href="${exercise.video}" target="_blank">Ver video en YouTube</a>
+                `;
+                muscleDiv.appendChild(exerciseDiv);
+            });
+
+            resultsContainer.appendChild(muscleDiv);
+        }
+    }
+}
